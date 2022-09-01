e75f8fbf2be39d1655140b90751ca4c0e3e5b6f6
Arissa Yasmin Iwashiro Otsu <arissaotsu@gmail.com>
2022-09-01 20:36:25 -0300

    Início homepage

--- projeto-pokedex/src/Pages/HomePage.js
+++ projeto-pokedex/src/Pages/HomePage.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import useRequestData from "../hooks/useRequestData";
 import * as MyRoutes from '../Routes/Coordinator'
+import { base_url } from "../constants/constants";
 
 function ShowHomePage () {
 
     const navigate = useNavigate()
+    const [pokemonsList, isLoadingPokemonsList, errorPokemonsList] = useRequestData(`${base_url}pokemon/`)
 
+    const renderPokemonsList = pokemonsList&&pokemonsList.results.map((pokemon) => {
+        return <div key={pokemon.name}>
+            <p> {pokemon.name} </p>
+        </div>
+    })
     return (
         <div>
             <h1> Página Inicial </h1>
             <button onClick={() => MyRoutes.goToPokedexPage(navigate)}> Ver Pokédex </button>
+            {renderPokemonsList}
         </div>
     )
 }
